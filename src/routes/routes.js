19ded7f7b2211e5b36b4a7396beb94625d82032b
Hiedi Ferreira <hiedyferreira@gmail.com>
2024-08-04 19:35:22 -0300

    adicionando Swagger ao projeto

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,8 @@
 const { Router } = require('express')
 
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('./doc.swagger.json')
+
 //Importando as rotas
 const usuariosRoutes = require('./usuarios.routes')
 const loginRoutes = require('./login.routes')
@@ -7,6 +10,8 @@ const locaisRoutes = require('./locais.routes')
 const validaToken = require('../middlewares/validaToken')
 
 const routes = new Router()
+
+routes.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 //Rotas públicas
 routes.use('/usuario', usuariosRoutes)
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -3,7 +3,35 @@ const UsuarioController = require('../controllers/UsuarioController')
 
 const usuariosRoutes = new Router()
 
-usuariosRoutes.post('/', UsuarioController.criarConta)
-usuariosRoutes.get('/', UsuarioController.listar)
+usuariosRoutes.post('/', UsuarioController.criarConta
+    /*
+        #swagger.tags = ['Usuarios'],
+        #swagger.description = 'Endpoint para cadastrar um novo usuário',
+        #swagger.parameters['novoUsuario'] = {
+            in: 'body',
+            description: 'Informações do usuário',
+            required: true,
+            schema: {
+                $nome: 'Fulano',
+                $data_nascimento: '2020-08-05',
+                $sexo: 'Feminino, Masculino OU Não informado',
+                $cpf: '00011122233 - 11 dígitos sem pontos',
+                $email: 'nome@email.com',
+                $cep: '00000111 - 8 dígitos sem traço',
+                $senha: '8 a 16 digitos'
+            }
+
+        }
+    */
+)
+usuariosRoutes.get('/', UsuarioController.listar
+    /*
+        #swagger.tags = ['Usuarios'],
+        #swagger.description = 'Endpoint para listar os usuários cadastrados no sistema',
+        #swagger.parameters['listarUsuario'] = {
+            description: 'Listagem de todos os usuários cadastrados no sistema.'
+        }
+    */
+)
 
 module.exports = usuariosRoutes
